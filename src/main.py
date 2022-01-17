1d73db17ed90c0b9ecc1d886d0391bbd3953798e
bottlecollector <bottlecollector52@gmail.com>
2022-01-17 15:49:50 +0000

    minor changes

--- src/main.py
+++ src/main.py
@@ -2,10 +2,7 @@
 
 from util import *
 
-
 if __name__ == "__main__":
-    print("hello world")
-
-    vertices, edges = get_data('FRH_P_toro.graph')
+    vertices, edges = get_data('../FRH_P_toro.graph')
 
     print(vertices)
