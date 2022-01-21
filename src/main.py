7f085c4153130a193a6c6801412f5369a399eb97
bottlecollector <bottlecollector52@gmail.com>
2022-01-21 19:31:06 +0000

    trynna draw line segments

--- src/main.py
+++ src/main.py
@@ -5,9 +5,14 @@ import numpy as np
 # import matplotlib.pyplot as plt
 
 from util import *
+from graphplotter import GraphPlotter
 
 if __name__ == "__main__":
     vertices, edge_ids, edges, edges_covariance = get_data(os.path.join(os.path.dirname(__file__), '../FRH_P_toro.graph'))
-    # print(vertices)
 
     draw_data_graph(vertices, edge_ids, edges, edges_covariance)
+
+    # Pygame attempt - kinda shitty cause zooms and stuff
+    # plotter = GraphPlotter()
+    # while 1:
+        # plotter.draw_data_graph(vertices, edge_ids, edges, edges_covariance)
--- src/util.py
+++ src/util.py
@@ -22,13 +22,14 @@ def get_data(filename):
                 # TORO file edge format:
                 # EDGE2 IDout IDin dx dy dth I11 I12 I22 I33 I13 I23
                 _, IDout, IDin, dx, dy, dth, I11, I12, I22, I33, I13, I23 = item
-                edge_ids.append((IDout, IDin))
-                edges.append((dx, dy, dth))
+                edge_ids.append((int(IDout), int(IDin)))
+                edges.append((float(dx), float(dy), float(dth)))
                 covar = [
                     [I11, I12, I13],
                     [I12, I22, I23],
                     [I13, I23, I33]
                 ]
+                # TODO(gonk): isn't append wrong, we should do vstack i think - check this
                 edges_covariance.append(covar)
 
     vertices = np.array(vertices)
@@ -38,10 +39,28 @@ def get_data(filename):
 
     return vertices, edge_ids, edges, edges_covariance
 
+import pylab as pl
+from matplotlib import collections as mc
+def draw_data_graph(vertices, edge_ids, edges, edge_covariance):
+    plt.scatter(vertices[:, 0], vertices[:, 1], s = 0.5)
+    plt.xlabel("X");plt.ylabel("Y")
+    plt.axis('off')
 
-def draw_data_graph(vertices, edge_id, edges, edge_covariance):
-    plt.scatter(vertices[:, 0], vertices[:, 1])
-    plt.xlabel("X")
-    plt.ylabel("Y")
+    # Plot edges
+    # for id1, id2 in edge_ids:
+        # x1, y1, _ = vertices[id1]
+        # x2, y2, _ = vertices[id2]
+        # plt.plot([x1], [y1], [x2], [y2], marker='o')
+
+    lines = []
+    for id1, id2 in edge_ids:
+        lines.append((vertices[id1][:2],vertices[id2][:2]))
+    lc = mc.LineCollection(lines, linewidths=2)
+    fig, ax = pl.subplots()
+    ax.add_collection(lc)
+    ax.autoscale()
+    ax.margins(0,1)
+
+    print('cum')
     plt.show()
-    # plt.savefig(os.path.join(os.path.dirname(__file__), 'plot.pdf'))
+
