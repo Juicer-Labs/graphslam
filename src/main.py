121481444ad7b9d342695dca138a109b1ffc9925
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-01-28 17:14:40 +0000

    notes

--- src/main.py
+++ src/main.py
@@ -10,4 +10,3 @@ if __name__ == "__main__":
     vertices, edge_ids, edges, edges_covariance = get_data(os.path.join(os.path.dirname(__file__), '../data/input_INTEL_g2o.g2o'))
 
     draw_data_graph(vertices, edge_ids, edges, edges_covariance)
-f
