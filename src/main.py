74c19064481b322e2814b4c25d50ce34ff61d238
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-01-28 17:03:48 +0000

    fix path

--- src/main.py
+++ src/main.py
@@ -6,7 +6,8 @@ import numpy as np
 from util import *
 
 if __name__ == "__main__":
-    # vertices, edge_ids, edges, edges_covariance = get_data(os.path.join(os.path.dirname(__file__), '../FRH_P_toro.graph'))
-    vertices, edge_ids, edges, edges_covariance = get_data(os.path.join(os.path.dirname(__file__), '../input_INTEL_g2o.g2o'))
+    # vertices, edge_ids, edges, edges_covariance = get_data(os.path.join(os.path.dirname(__file__), '../data/FRH_P_toro.graph'))
+    vertices, edge_ids, edges, edges_covariance = get_data(os.path.join(os.path.dirname(__file__), '../data/input_INTEL_g2o.g2o'))
 
     draw_data_graph(vertices, edge_ids, edges, edges_covariance)
+f
