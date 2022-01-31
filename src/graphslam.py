009aa4038332ee97cf4330cda10ac100eb9e1bb7
bottlecollector <bottlecollector52@gmail.com>
2022-01-31 20:45:57 +0000

    finished msot pseudocode for backend BUT ERROR MATRIX IS SINGULAR??

--- src/graphslam.py
+++ src/graphslam.py
@@ -26,36 +26,57 @@ def get_information_matrix(covars):
     return result
 
 def graphslam(x, info_matrix, edge_ids, edges):
-    n = len(x)*3
+    n = len(x)
     for i in range(10): # 'while not converged'
         print('iteration numba: {}'.format(i))
 
-        b = np.zeros((n), dtype=np.float32)
-        H = np.zeros((n, n), dtype=np.float32)
+        b = np.zeros((n, 3), dtype=np.float32)
+        H = np.zeros((n, n, 3,3), dtype=np.float32)
         # for all connected edges
         for e_count, (i, j) in enumerate(edge_ids):
             A_ij = np.identity(3)
             B_ij = -np.identity(3)
 
-            H[i:i+3, i:i+3] += np.dot(A_ij.T, np.dot(info_matrix[i, j], A_ij))
-            H[i:i+3, j:j+3] += np.dot(A_ij.T, np.dot(info_matrix[i, j], B_ij))
-            H[j:j+3, i:i+3] += np.dot(B_ij.T, np.dot(info_matrix[i, j], A_ij))
-            H[j:j+3, j:j+3] += np.dot(B_ij.T, np.dot(info_matrix[i, j], B_ij))
+            # H[i:i+3, i:i+3] += np.dot(A_ij.T, np.dot(info_matrix[e_count], A_ij))
+            # H[i:i+3, j:j+3] += np.dot(A_ij.T, np.dot(info_matrix[e_count], B_ij))
+            # H[j:j+3, i:i+3] += np.dot(B_ij.T, np.dot(info_matrix[e_count], A_ij))
+            # H[j:j+3, j:j+3] += np.dot(B_ij.T, np.dot(info_matrix[e_count], B_ij))
+            H[i, i] += np.dot(A_ij.T, np.dot(info_matrix[e_count], A_ij))
+            H[i, j] += np.dot(A_ij.T, np.dot(info_matrix[e_count], B_ij))
+            H[j, i] += np.dot(B_ij.T, np.dot(info_matrix[e_count], A_ij))
+            H[j, j] += np.dot(B_ij.T, np.dot(info_matrix[e_count], B_ij))
 
             error = error_func(x[i], x[j], edges[e_count])
-            print('error', error)
-            print(np.dot(A_ij.T, np.dot(info_matrix[i, j], error)))
-            print(A_ij)
-            print(A_ij.T)
-            b[i:i+3] += np.dot(A_ij.T, np.dot(info_matrix[i, j], error))
-            b[j:j+3] += np.dot(B_ij.T, np.dot(info_matrix[i, j], error))
 
-        H[0:3,0:3] += np.identity(3) # Keep first node fixed
+            # print '\nerror', error
+            # print '\nresult\n', np.dot(A_ij.T, np.dot(info_matrix[e_count], error))
+            # print('\nA_ij\n', A_ij)
+            # print('\nA_ij.T\n', A_ij.T)
+            # print '\nb\n', b
+
+            # print 'info x error'
+            # print np.dot(info_matrix[e_count], error)
+            # print 'info mat'
+            # print info_matrix[e_count]
+
+            # print 'info mat length ' + str(len(info_matrix)) + ' n= ' + str(n)
+            # print 'cum'
+            # print info_matrix[e_count]
+
+            b[i] += np.dot(A_ij.T, np.dot(info_matrix[e_count], error))
+            b[j] += np.dot(B_ij.T, np.dot(info_matrix[e_count], error))
+
+        print 'cum'
+        print H[0,0]
+        H[0,0] += np.identity(3) # Keep first node fixed
+        print H[0,0]
+        # print 'det of H: ' + str(np.linalg.det(H.reshape(n*3, n*3)))
+        # print 'shape of H: ' + str(H.reshape(n*3, n*3).shape)
         # use sparse solver instead
-        delta_x = np.dot(np.linalg.inv(H), -b)
+        delta_x = np.dot(np.linalg.inv(H.reshape(n*3, n*3)), -b)
         x += delta_x
 
-    H[0:3,0:3] -= np.identity(3) # Keep first node fixed
+    H[0,0] -= np.identity(3) # Keep first node fixed
     return x, H
 
 
